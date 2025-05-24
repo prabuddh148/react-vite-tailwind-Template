9801c0db35c4e7f9e4f67b50703d1716d3065d2c
Prabuddh <prabuddh148>
2025-05-25 00:12:50 +0530

    changes

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,4 @@
-import InstaIcon from "../assets/insta-icon.png";
-import YoutubeIcon from "../assets/youtube-icon.png";
-import FacebookIcon from "../assets/facebook-icon.png";
-import LinkedIcon from "../assets/linkedin-icon.png";
+
 import { useNavigate } from "react-router-dom";
 
 export const Footer = () => {
